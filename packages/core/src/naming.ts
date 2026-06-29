3e9ce366377e268d7c65997fa242fc7491ca39b1
greatweir-star <greatweir@gmail.com>
2026-06-29 09:55:53 +0800

    chore(core): add product naming constants

---
+++ packages/core/src/naming.ts
@@ -0,0 +1,4 @@
+export const PRODUCT_NAME_CN = "灵机工坊";
+export const PRODUCT_NAME_EN = "LumiForge";
+export const PRODUCT_NAME = `${PRODUCT_NAME_CN} ${PRODUCT_NAME_EN}`;
+export const PACKAGE_SCOPE = "@lumiforge";
